4d806f0d253f562ba2475b00456ccc65845afb90
Erikson Gonçalves Sanchez <eriksongoncalves@yahoo.com.br>
2025-11-29 18:24:41 -0300

    feat: add initial pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,19 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
 import "./globals.css";
+import { Sidebar } from "@/app/_components/sidebar";
+
+const inter = Inter({
+  display: "auto",
+  variable: "--font-inter",
+  subsets: ["latin"]
+});
+
+export const metadata: Metadata = {
+  title: "Stockly",
+  description: ""
+};
 
 export default function RootLayout({
   children
@@ -6,8 +21,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+    <html lang="pt-br">
+      <body className={`${inter.className} antialiased`}>
+        <div className="flex h-full gap-8">
+          <Sidebar />
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
